944d2c49dc64b126e85568fc3912e640e98f12c9
Erin Davis <davise25@winthrop.edu>
2021-11-14 16:27:55 -0500

    updating path

--- backend/index.js
+++ backend/index.js
@@ -156,6 +156,12 @@ app.post('/api/withdraw', async (req, res) => {
 
 const port = process.env.PORT || 3001;
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.listen(port, function() {
     console.log("Server runnning on " + port);
   });
