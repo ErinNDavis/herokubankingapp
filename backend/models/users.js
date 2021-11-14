baaf59823f78805175b6e075ce404175b720c999
Erin Davis <davise25@winthrop.edu>
2021-11-14 16:14:35 -0500

    initial

---
+++ backend/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const model = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = new mongoose.model("Users", model)
